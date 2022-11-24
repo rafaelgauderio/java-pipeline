9a624e56a27dc43862abe6e4e876d326f92ebc8a
Rafael de Luca <deluca1712@gmail.com>
2022-11-24 11:13:56 -0300

    commenting the pipeline operations

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,18 +11,21 @@ public class Program {
 
 		Locale.setDefault(Locale.US);
 
-		List<Integer> list = Arrays.asList(1, 3, 5, 6, 9, 15, 21,26, 32, 36, 40);
-		Stream<Integer> stream01 = list.stream().map(x -> x * 15);
-		System.out.println(Arrays.toString(stream01.toArray()));
-
+		List<Integer> list = Arrays.asList(1, 3, 5, 7, 9, 10, 11, 13,15, 17, 19, 40);
+		Stream<Integer> stream01 = list.stream().map(x -> x * 15); //map() is intermediary operation
+		System.out.println(Arrays.toString(stream01.toArray())); // toArray() is a final operation
+		
 		int sum = list.stream().reduce(0, (x, y) -> x + y);
 		System.out.println("Summation= " + sum);
 
-		int prod = list.parallelStream().reduce(0, (x, y) -> x * y);
-		System.out.println("Productive= " + prod);
+		int prod = list.parallelStream().reduce(1, (x, y) -> x * y);
+		System.out.println("Producer= " + prod);
 
-		System.out.println("Filer by even number and multiply by five");
-		List<Integer> newList = list.stream().filter(x -> x % 2 == 0).map(x -> x * 5).collect(Collectors.toList());
+		System.out.println("Filter by even number and multiply by 5");
+		List<Integer> newList = list.stream().
+				filter(x -> x % 2 == 0). // filter the stream
+				map(x -> x * 5). // apply the map function on the result stream
+				collect(Collectors.toList()); // converter stream to List
 		System.out.println(Arrays.toString(newList.toArray()));
 
 	}
